8b449d0c7af209f5b7b0e99f6412a037afe1d866
shminge <77525090+shminge@users.noreply.github.com>
2025-07-09 23:58:19 +1000

    construction note

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <main style={{ padding: '2rem' }}>
             <h1>My Blog</h1>
+            <p>This is heavily under construction. Check back later!</p>
             <ul>
                 <li><Link href="/blog/hello-world">Hello World</Link></li>
             </ul>
