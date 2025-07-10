e7e44838bce5dada9e7c52ee10ddb670352e84df
shminge <77525090+shminge@users.noreply.github.com>
2025-07-10 10:46:32 +1000

    initial css setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <main style={{ padding: '2rem' }}>
-            <h1>My Blog</h1>
+            <h1 className="bg-red-500 text-white p-4 rounded">My Blog</h1>
             <p>This is heavily under construction. Check back later!</p>
             <ul>
                 <li><Link href="/blog/hello-world">Hello World</Link></li>
